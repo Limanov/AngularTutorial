57dcf1ee989b4cab19079ad887d7fbe12496b3de
Kamil Kalota <kamilkalota@gmail.com>
2018-07-06 20:20:20 +0200

    apka z tutoriala angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HerosiComponent } from './herosi/herosi.component';
+import { DetaleHerosaComponent } from './detale-herosa/detale-herosa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosiComponent,
+    DetaleHerosaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
